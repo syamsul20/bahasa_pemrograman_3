aea89ceef4bb26db9e8e3df05e63829fedd8c28f
syamsul20 <114728043+syamsul20@users.noreply.github.com>
2022-10-11 16:37:21 +0700

    Push dari upload github

---
+++ tugassesi3.py
@@ -0,0 +1,51 @@
+#type data di python
+#type data integer
+alva=12
+print (alva)
+print (type(alfa))
+
+#type data float
+bepa=12.5
+print (bepa)
+print (type(bepa))
+
+#type data complex
+bahasa=12j
+print(bahasa)
+print (type(bahasa))
+
+#type data string
+A="hello world"
+print (A)
+print (type(A))
+
+#type data tuple
+B=("A","B","C")
+C= 12,5,3,1
+print (B,C)
+print (type(B),type (C))
+
+#print type data lis
+D=["A","B","C"]
+print (D)
+print (type(D))
+
+a=1
+a=2
+print(a)
+
+#type data set
+D= ("Saya","Sedang","Kuliah")
+print (D)
+print (type(D))
+
+#type data dictonary
+D={"nama":"Syamsul","umur":20}
+print = (D)
+print (type(D))
+
+#type data bool
+a=True
+b=False
+print (a,b)
+print (type(a), type(b))
